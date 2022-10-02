c9b5435e25931b8232d83dca11aad91edb80399d
sindhumarri <39763794+sindhumarri@users.noreply.github.com>
2022-10-02 18:32:40 +0530

    file accept changes updated

--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['userData'])) {
 <body>
 	<label> Please upload here: </label>
 	<!-- For Single File: -->
-	<input type="file" name="image" accept="image/*" id="imageButton" />
+	<input type="file" name="image" id="imageButton" />
 	<!-- For Multiple Files: -->
 	<!-- <input type="file" name="image[]" accept="image/*" id="imageButton" multiple/> -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
